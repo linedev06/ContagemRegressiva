42aad7e4e6b06e736f3acb8cda0d9ea5d2b6a8ad
linedev06 <vitorafelismino@gmail.com>
2023-03-10 20:36:55 -0300

    exercicio contagem regressiva resolvido

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("contagem regressiva");
+Console.WriteLine("preparar...");
+Console.WriteLine("apontar...");
+Console.WriteLine("fogo!!!");
